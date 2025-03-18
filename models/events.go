782f6be1e2a16eca00ead603b6e357246b3e0e6b
Talhah Peerbhai <talhah.peerbhai@gmail.com>
2025-03-18 23:11:24 +0300

    Fix event timings

--- models/events.go
+++ models/events.go
@@ -9,14 +9,14 @@ type Event struct {
 	Name            string    `gorm:"size:255;not null" json:"name"`
 	Description     string    `gorm:"type:text" json:"description"`
 	Location        string    `gorm:"size:255" json:"location"`
-	startTime        time.Time `gorm:"not null" json:"start_time"`
-  endTime        time.Time `gorm:"not null" json:"end_time"`
+	StartTime       *time.Time `gorm:"type:timestamptz" json:"start_time"` // Pointer to time.Time, allows NULL
+	EndTime         *time.Time `gorm:"type:timestamptz" json:"end_time"`   // Pointer to time.Time, allows NULL
 	OrganizerID     uint      `gorm:"not null" json:"organizer_id"` // ID of the user who organized the event
 	PointsAllocation int      `gorm:"default:0" json:"points_allocation"`
-  ImageURL     string    `gorm:"size:512" json:"icon_url"`
+	ImageURL        string    `gorm:"size:512" json:"icon_url"`
 
 	// Relationships
-	Organizer User       `gorm:"foreignKey:OrganizerID" json:"organizer"`
+	Organizer User         `gorm:"foreignKey:OrganizerID" json:"organizer"`
 	Attendees []Attendance `gorm:"foreignKey:EventID" json:"attendees"`
-	Awards    []Award     `gorm:"many2many:event_awards" json:"awards"` // Many-to-many relationship with awards
+	Awards    []Award      `gorm:"many2many:event_awards" json:"awards"` // Many-to-many relationship with awards
 }
